fb2c91f08855831bb9c3d9ee3406db8a65924eb8
Miguel Julio <fitfab@yahoo.com>
2016-09-06 23:38:45 -0400

    add a color palete

---
+++ client/firebase.config.js
@@ -0,0 +1,6 @@
+export default {
+   apiKey: "AIzaSyC1zeANABF6j-HuWXcZ7ll4pp3saZQBHgw",
+   authDomain: "fitfab-3f08a.firebaseapp.com",
+   databaseURL: "https://fitfab-3f08a.firebaseio.com",
+   storageBucket: ""
+}
--- client/products/components/products.js
+++ client/products/components/products.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 export default React.createClass({
   render(){
-    console.log('This Products Props : ', this.props);
     return (
       <h2>Product Page Component</h2>
     )
